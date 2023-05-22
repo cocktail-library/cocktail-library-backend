8a417dc71e02bffab9b85a307e76ee04a4cd3dbf
fr1tsl3r <fr1tsl3r@gmail.com>
2023-05-22 22:50:48 +0500

    LIB-8 add .env copy during deploy

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 export DOCKER_CONFIG="${HOME}/.docker-cocktail-library-backend"
 # on first deploy, go to machine and enter docker login here
 # docker login -u <TOKEN> -p <TOKEN> registry.fritsler.ru
+cp ../.env.backend .env
 mv -f ./deploy/docker-compose.yaml .
 docker compose pull
 # todo remove old images
