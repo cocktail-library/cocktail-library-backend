7c0c06354b34511b6ab86dfb6216b1e5341feba4
fr1tsl3r <fr1tsl3r@gmail.com>
2023-05-23 16:51:53 +0500

    LIB-16 WIP, update cocktail repository

--- src/cocktails/cocktails.entity.ts
+++ src/cocktails/cocktails.entity.ts
@@ -1,14 +1,26 @@
 import { DataTypes, Sequelize } from 'sequelize'
 import { nanoid } from 'nanoid'
+import { ITag } from '../tags/tags.entity'
+import { IReview } from '../reviews/reviews.entity'
+import { IIngredient } from '../ingredients/ingredients.entity'
+
+interface ICocktailIngredient {
+  ingredient: IIngredient;
+  unitCount: number;
+}
 
 interface ICocktail {
   id: number;
+  cocktailId: string;
   tagId: string;
   name: string;
   description: string;
   abv: number;
   createdAt: Date;
   updatedAt: Date;
+  tags?: ITag[];
+  reviews?: IReview[];
+  ingredients?: ICocktailIngredient[];
 }
 
 const CocktailsEntity = (sequelize: Sequelize) => sequelize.define('Cocktail', {
@@ -40,5 +52,6 @@ const CocktailsEntity = (sequelize: Sequelize) => sequelize.define('Cocktail', {
 
 export {
   CocktailsEntity as Cocktail,
-  ICocktail
+  ICocktail,
+  ICocktailIngredient,
 }
--- src/cocktails/cocktails.repository.ts
+++ src/cocktails/cocktails.repository.ts
@@ -1,7 +1,9 @@
 import { sequelize } from '../db-connection'
 import { GenericRepository } from '../generic/generic.repository'
-import { Cocktail, ICocktail } from './cocktails.entity'
+import { Cocktail, ICocktail, ICocktailIngredient } from './cocktails.entity'
 import { injectable } from 'inversify'
+import { ITag } from '../tags/tags.entity'
+import { IReview } from '../reviews/reviews.entity'
 
 @injectable()
 class CocktailsRepository {
@@ -11,25 +13,72 @@ class CocktailsRepository {
   }
 
   async listAll(offset = 0, limit = 100, where = {}) {
-    return this.baseRepository.listAll(offset, limit, where)
+    const cocktailsData = await this.baseRepository.listAll<ICocktail>(offset, limit, where)
+
+    const cocktailIds = cocktailsData.result.map(cocktail => cocktail.cocktailId)
+    const tagRecords = await this.listTagsByCocktailIds(cocktailIds)
+    const reviewRecords = await this.listReviewsByCocktailIds(cocktailIds)
+    const ingredientRecords = await this.listIngredientsByCocktailIds(cocktailIds)
+
+    return {
+      ...cocktailsData,
+      result: cocktailsData.result.map(cocktailData => ({
+        ...cocktailData,
+        tags: tagRecords[cocktailData.cocktailId] || [],
+        reviews: reviewRecords[cocktailData.cocktailId] || [],
+        ingredients: ingredientRecords[cocktailData.cocktailId] || [],
+      }))
+    }
   }
 
-  async get(cocktailId: string) {
-    return await this.baseRepository.get(cocktailId)
+  async get(cocktailId: string): Promise<ICocktail> {
+    const cocktailData = await this.baseRepository.get(cocktailId)
+
+    const tagRecords = await this.listTagsByCocktailIds([cocktailId])
+    const reviewRecords = await this.listReviewsByCocktailIds([cocktailId])
+    const ingredientRecords = await this.listIngredientsByCocktailIds([cocktailId])
+
+    return {
+      ...cocktailData,
+      tags: tagRecords[cocktailId] || [],
+      reviews: reviewRecords[cocktailId] || [],
+      ingredients: ingredientRecords[cocktailId] || [],
+    }
   }
 
   async update(cocktailId: string, payload: Partial<ICocktail>) {
     await this.baseRepository.update(cocktailId, payload)
+    // TODO update related entities
     return await this.get(cocktailId)
   }
 
   async create(payload: Partial<ICocktail>) {
     const cocktail = await this.baseRepository.create(payload)
+    // TODO update related entities
     return await this.get(cocktail.cocktailId)
   }
 
   async delete(cocktailId: string) {
+    // TODO update related entities
     await this.baseRepository.delete({ cocktailId })
+  }
+
+  private async listTagsByCocktailIds(cocktailIds: string[]): Promise<Record<string, ITag[]>> {
+    const result: Record<string, ITag[]> = {}
+    // TODO perform SQL query
+    return result
+  }
+
+  private async listReviewsByCocktailIds(cocktailIds: string[]): Promise<Record<string, IReview[]>> {
+    const result: Record<string, IReview[]> = {}
+    // TODO perform SQL query
+    return result
+  }
+
+  private async listIngredientsByCocktailIds(cocktailIds: string[]): Promise<Record<string, ICocktailIngredient[]>> {
+    const result: Record<string, ICocktailIngredient[]> = {}
+    // TODO perform SQL query
+    return result
   }
 }
 
